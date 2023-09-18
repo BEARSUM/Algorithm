c5e97cdc646d0f5bf86156682f88ec857c710c3b
BEARSUM <125293472+BEARSUM@users.noreply.github.com>
2023-09-18 16:09:51 +0900

    [unrated] Title: n보다 커질 때까지 더하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.js
+++ 프로그래머스/unrated/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.js
@@ -1,8 +1,4 @@
 function solution(numbers, n) {
-    let result=0;
-    for(let i=0; i<numbers.length; i++){
-        result+=numbers[i];
-        if(result>n) return result;
-    }
-                   
+    return numbers.reduce((acc,cur)=> acc>n ? acc : acc+cur);
+    
 }
