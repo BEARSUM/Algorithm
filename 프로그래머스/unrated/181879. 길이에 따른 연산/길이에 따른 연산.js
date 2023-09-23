7e0925ed39282425ef12032e1883fb0071ddb0c1
BEARSUM <125293472+BEARSUM@users.noreply.github.com>
2023-09-23 22:21:19 +0900

    [unrated] Title: 길이에 따른 연산, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181879. 길이에 따른 연산/길이에 따른 연산.js
+++ 프로그래머스/unrated/181879. 길이에 따른 연산/길이에 따른 연산.js
@@ -1,5 +1,3 @@
 function solution(num_list) {
-    return num_list.length>10 ? 
-        num_list.reduce((a,c)=>a+c):
-    num_list.reduce((a,c)=>a*c)
+    return num_list.reduce((a,c)=>num_list.length>10 ?a+c:a*c);
 }
