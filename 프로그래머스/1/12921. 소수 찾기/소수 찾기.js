65c4578ddd805274f8e5dc03cf434bfa1f54c981
BEARSUM <125293472+BEARSUM@users.noreply.github.com>
2023-11-18 13:31:48 +0900

    [level 1] Title: 소수 찾기, Time: 31.90 ms, Memory: 45 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.js
@@ -0,0 +1,21 @@
+function solution(n) {
+  const isPrime = new Array(n + 1).fill(true);
+  isPrime[0] = isPrime[1] = false;
+
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (isPrime[i]) {
+      for (let j = i * i; j <= n; j += i) {
+        isPrime[j] = false;
+      }
+    }
+  }
+
+  const primes = [];
+  for (let i = 2; i <= n; i++) {
+    if (isPrime[i]) {
+      primes.push(i);
+    }
+  }
+
+  return primes.length;
+}
