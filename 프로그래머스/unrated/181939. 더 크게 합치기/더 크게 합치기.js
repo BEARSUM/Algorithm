3c4f1b2d626efd30721a2cc43eb9b53101fe1390
BEARSUM <125293472+BEARSUM@users.noreply.github.com>
2023-11-19 20:10:33 +0900

    [level unrated] Title: 더 크게 합치기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.js
+++ 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.js
@@ -1,8 +1,3 @@
 function solution(a, b) {
-    const stringA=String(a);
-    const stringB=String(b);
-    const sumAB=stringA+stringB;
-    const sumBA=stringB+stringA;
-    
-    return sumAB>sumBA ? sumAB*1:sumBA*1;
+    return Math.max(`${a}${b}`,`${b}${a}`)
 }
