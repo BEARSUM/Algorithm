68939c393d5d786143366bab1c749e659a6fc429
BEARSUM <125293472+BEARSUM@users.noreply.github.com>
2023-11-23 18:18:01 +0900

    [level unrated] Title: 문자열 바꿔서 찾기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.js
+++ 프로그래머스/unrated/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.js
@@ -1,5 +1,8 @@
 function solution(myString, pat) {
-    const reverseString=myString.split('').map((el)=>el==="A"?"B":"A").join('');
-    const newArr = reverseString.split(pat);
-    return newArr.length>1 ?  1 : 0;
+    // const reverseString=myString.split('').map((el)=>el==="A"?"B":"A").join('');
+    // const newArr = reverseString.split(pat);
+    // return newArr.length>1 ?  1 : 0;
+    
+    const reverseString=[...myString].map((el)=>el==="A"?"B":"A").join('');
+    return reverseString.includes(pat) ? 1 : 0;
 }
