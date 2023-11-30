e2251788fe754fcbfa266be2270cd8da948c0220
BEARSUM <125293472+BEARSUM@users.noreply.github.com>
2023-11-30 23:57:56 +0900

    [level unrated] Title: 가까운 1 찾기, Time: 0.02 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.js
+++ 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.js
@@ -1,8 +1,14 @@
 function solution(arr, idx) {
-    const newArray =[...arr.splice(idx)];
+    //     solution-1
+    //     const newArray =[...arr.splice(idx)];
+
+    //     if(!newArray.includes(1)) return -1;
+    //     for(let i=0; i<newArray.length; i++){
+    //         if(newArray[i]===1) return i+idx;
+    //     }
     
-    if(!newArray.includes(1)) return -1;
-    for(let i=0; i<newArray.length; i++){
-        if(newArray[i]===1) return i+idx;
-    }
+    //     solution-2
+    return arr.indexOf(1,idx);
+    
+    
 }
