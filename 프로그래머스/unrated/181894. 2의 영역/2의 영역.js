5442f460abbc6298eac5d4be979680641534b6c5
BEARSUM <125293472+BEARSUM@users.noreply.github.com>
2023-12-03 14:48:03 +0900

    [level unrated] Title: 2의 영역, Time: 0.48 ms, Memory: 41.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181894. 2의 영역/2의 영역.js
+++ 프로그래머스/unrated/181894. 2의 영역/2의 영역.js
@@ -1,10 +1,18 @@
 function solution(arr) {
-    let index = [];
-    if(!arr.includes(2)) return [-1];
-    else {
-        arr.map((el,i)=>{
-            if(el===2) index.push(i);
-        })
-    }
-    return arr.slice(index[0],index[index.length-1]+1)
+    // solution 1
+    // let index = [];
+    // if(!arr.includes(2)) return [-1];
+    // else {
+    //     arr.map((el,i)=>{
+    //         if(el===2) index.push(i);
+    //     })
+    // }
+    // return arr.slice(index[0],index[index.length-1]+1)
+    
+    // solution 2
+    const from = arr.indexOf(2);
+    const to = arr.lastIndexOf(2);
+    
+    if(from===-1) return [-1];
+    return arr.slice(from,to+1);
 }
