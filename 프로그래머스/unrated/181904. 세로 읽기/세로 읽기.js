fbedac9a85eadaf93cbff2ed619bc79a9dbfd406
BEARSUM <125293472+BEARSUM@users.noreply.github.com>
2023-12-11 16:30:38 +0900

    [level unrated] Title: 세로 읽기, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.js
+++ 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.js
@@ -1,8 +1,12 @@
 function solution(my_string, m, c) {
-    const count = my_string.length/m;
-    let answer='';
-    for(let i=0; i<count; i++){
-        answer+=my_string[(c-1)+m*i];
-    }
-    return answer;
+    //sol1
+    // const count = my_string.length/m;
+    // let answer='';
+    // for(let i=0; i<count; i++){
+    //     answer+=my_string[(c-1)+m*i];
+    // }
+    // return answer;
+    
+    //sol2
+    return [...my_string].filter((_,i)=>i%m===c-1).join('');
 }
