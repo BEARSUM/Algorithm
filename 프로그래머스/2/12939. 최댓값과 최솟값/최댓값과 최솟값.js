69bb068898105c999bbb01c5ecbfccbf523616c6
BEARSUM <125293472+BEARSUM@users.noreply.github.com>
2023-12-13 20:35:12 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -1,4 +1,8 @@
 function solution(s) {
-    const numbers = s.split(' ').sort((a,b)=>(+a)-(+b));
-    return numbers[0]+' '+numbers[numbers.length-1]
+    // const numbers = s.split(' ').sort((a,b)=>(+a)-(+b));
+    // return numbers[0]+' '+numbers[numbers.length-1]
+    
+    //sol2
+    const numbers = s.split(' ');
+    return Math.min(...numbers)+' '+Math.max(...numbers)
 }
