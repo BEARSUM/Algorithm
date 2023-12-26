ae00dce998fe46140eb81d2dff5767808b9d89de
BEARSUM <125293472+BEARSUM@users.noreply.github.com>
2023-12-27 00:01:57 +0900

    [level unrated] Title: 배열 뒤집기, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120821. 배열 뒤집기/배열 뒤집기.js
+++ 프로그래머스/unrated/120821. 배열 뒤집기/배열 뒤집기.js
@@ -1,10 +1,3 @@
 function solution(num_list) {
-    //1.뒤에서 부터 하나씩 꺼낸다.
-    //2.새 배열에 넣어준다.
-    let answer =[];
-    for(let i=num_list.length-1; i>=0; i--){
-        let item = num_list[i];
-        answer.push(item);
-    }
-    return answer;
+    return num_list.reverse();
 }
