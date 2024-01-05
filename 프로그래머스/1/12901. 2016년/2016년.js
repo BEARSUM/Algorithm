daa52986086ee16da5d6c0335c9569be2ceaac38
BEARSUM <125293472+BEARSUM@users.noreply.github.com>
2024-01-06 00:10:00 +0900

    [level 1] Title: 2016년, Time: 0.24 ms, Memory: 34.4 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.js
+++ 프로그래머스/1/12901. 2016년/2016년.js
@@ -1,7 +1,5 @@
-const DAY={0:'SUN',1:'MON',2:'TUE',3:'WED',4:'THU',5:'FRI',6:'SAT'};
-
 function solution(a, b) {
-    const curDay = new Date(`2016-${a}-${b}`).getDay();
-    return DAY[curDay];
+    const curDate = new Date(`2016-${a}-${b}`);
+    return curDate.toString().slice(0,3).toUpperCase();
     
 }
