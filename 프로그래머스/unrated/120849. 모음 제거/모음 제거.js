5d417be71ab3c638d50dc789c67b06ae65ce26ec
문수민 <125293472+BEARSUM@users.noreply.github.com>
2024-01-13 00:02:13 +0900

    [level unrated] Title: 모음 제거, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120849. 모음 제거/모음 제거.js
+++ 프로그래머스/unrated/120849. 모음 제거/모음 제거.js
@@ -1,21 +1,3 @@
-const mo = ['a','e','i','o','u'];
-
-
-
-
 function solution(my_string) {
-    let result='';
-    for(let j=0; j<my_string.length; j++){
-        let item = my_string[j];
-        let ismo = false;
-    for(let i=0; i<mo.length; i++){
-        if(item===mo[i]){
-        ismo=true;
-         }
-    }
-        if(!ismo){
-            result+=item;
-        }
-    }
-    return result;
+    return my_string.replace(/[aeiou]/g, '');
 }
