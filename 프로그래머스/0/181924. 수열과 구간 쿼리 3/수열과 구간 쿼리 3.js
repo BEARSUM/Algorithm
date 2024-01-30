5db9c6f1ee2782ba17f88d85ea26ff0d3b8e0dba
문수민 <125293472+BEARSUM@users.noreply.github.com>
2024-01-30 15:36:25 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 0.67 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.js
+++ 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.js
@@ -1,10 +1,8 @@
 function solution(arr, queries) {
     const answer=[...arr];
     
-    queries.map((el)=>{
-        const [x,y] = [answer[el[0]],answer[el[1]]]
-        answer[el[0]]=y;
-        answer[el[1]]=x;
+    queries.map(([x,y])=>{
+        [answer[x],answer[y]] = [answer[y],answer[x]];
     })
     
     return answer;
