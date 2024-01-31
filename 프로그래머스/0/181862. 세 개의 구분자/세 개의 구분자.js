69f116a4967f9c1f9364f230319906e1e11923bb
문수민 <125293472+BEARSUM@users.noreply.github.com>
2024-01-31 17:08:09 +0900

    [level 0] Title: 세 개의 구분자, Time: 15.65 ms, Memory: 52.6 MB -BaekjoonHub

--- 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.js
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.js
@@ -1,7 +1,5 @@
 function solution(myStr) {
-    const myStrWithoutAbc = myStr.replace(/[abc]/g,' ')
-                .replace(/\s+/g,' ')
-                .trim()
-                .split(' ');
-   return !!myStrWithoutAbc[0]?myStrWithoutAbc:["EMPTY"];
+    const myStrWithoutAbc = myStr.split(/[abc]/)
+                                 .filter(str=>str);
+   return myStrWithoutAbc.length?myStrWithoutAbc:["EMPTY"];
 }
