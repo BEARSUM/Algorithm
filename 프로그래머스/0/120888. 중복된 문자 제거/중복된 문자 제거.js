cb4d4b3a0a9facf7deafb014e4f7f033103c61b2
문수민 <125293472+BEARSUM@users.noreply.github.com>
2024-03-27 22:53:57 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.js
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.js
@@ -1,7 +1,4 @@
 function solution(my_string) {
-    const result =[];
-    [...my_string].forEach((char)=>{
-        if(!result.includes(char)) result.push(char);
-    })
-    return result.join('');
+    const result = new Set(my_string);
+    return [...result].join('')
 }
