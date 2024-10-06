bf2b9791685ffd9ae0d30f975a1c2ebdcff9ebf1
문수민 <125293472+BEARSUM@users.noreply.github.com>
2024-10-06 14:53:07 +0900

    [level 1] Title: 평균 구하기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12944. 평균 구하기/평균 구하기.js
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.js
@@ -1,11 +1,3 @@
 function solution(arr) {
-    // let sum =0;
-    // for(let i=0; i<arr.length;i++){
-    //     sum += arr[i];
-    // }
-    // const answer= sum/arr.length;
-    // return answer;
-    
-    //2번째 풀이
-    return arr.reduce((a,b)=>a+b)/arr.length;
+    return arr.reduce((acc,cur)=>acc+cur/arr.length,0);
 }
