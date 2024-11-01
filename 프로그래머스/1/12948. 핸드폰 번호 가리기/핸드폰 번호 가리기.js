4309c6b42eb9f168ca1ffdca356ab3aeba3ddba8
문수민 <125293472+BEARSUM@users.noreply.github.com>
2024-11-01 15:08:18 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.08 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,5 +1,6 @@
 function solution(phone_number) {
-    return [...phone_number].map((el,i)=>i>=phone_number.length-4?el:'*')
+    const length = phone_number.length;
+    
+    return [...phone_number].map((number,index)=>index<length-4?'*':number)
                             .join('');
-
 }
